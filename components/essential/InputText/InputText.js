311d8079378882909b3551b39c2035029e8bf060
Santiago Molto <sistemas1@persax.es>
2021-09-28 08:22:56 +0200

    Login form modified

--- components/essential/InputText/InputText.js
+++ components/essential/InputText/InputText.js
@@ -1,8 +1,8 @@
-export const InputText = ({ text, inputTextCSS, action, type, label, placeholder }) => {
+export const InputText = ({ text, inputTextCSS, action, type, label, placeholder, required }) => {
   return (
     <>
       <span>{label}</span>
-      <input type={type} className={inputTextCSS} value={text} onChange={action} placeholder={placeholder} />
+      <input type={type} className={inputTextCSS} value={text} onChange={action} placeholder={placeholder} required={required} />
     </>
   )
 }
--- pages/login.js
+++ pages/login.js
@@ -9,7 +9,8 @@ import styles from '../styles/Login.module.css'
 
 const Login = () => {
   const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [isLogin, setIsLogin] = useState(true)
+
   const onSubmit = (e) => {
     e.preventDefault()
     console.log('Hola mundo')
@@ -28,28 +29,47 @@ const Login = () => {
               src={'/logo.svg'}
               alt="Picture of the author"
             />
-            <span className={styles['login-title']}>Sign in to WhatsApp-Clone</span>
-            <InputText
-              type={'email'}
-              label={'E-mail: '}
-              text={email}
-              placeholder={'test@gmail.com'}
-              inputTextCSS={'input'}
-              action={(e) => setEmail(e.target.value)}
-            />
-            <InputText
-              type={'password'}
-              label={'Password: '}
-              text={password}
-              placeholder={''}
-              inputTextCSS={'input'}
-              action={(e) => setPassword(e.target.value)}
-            />
-            <Button
-              text={'Login'}
-              buttonCSS={'btn btn-primary'}
-              action={onSubmit}
-            />
+            {
+              isLogin === true
+                ? <>
+                  <span className={styles['login-title']}>Sign in to WhatsApp-Clone</span>
+                  <InputText
+                    type={'email'}
+                    label={'E-mail: '}
+                    text={email}
+                    placeholder={'test@gmail.com'}
+                    inputTextCSS={'input'}
+                    required={true}
+                    action={(e) => setEmail(e.target.value)}
+                  />
+                  <Button
+                    text={'Login'}
+                    buttonCSS={'btn btn-primary'}
+                    action={onSubmit}
+                  />
+                  <br />
+                  New to Whatsapp-clone? <a href='#' onClick={() => setIsLogin(false)}>Create an account</a>
+                </>
+                : <>
+                  <span className={styles['login-title']}>Sign up to WhatsApp-Clone</span>
+                  <InputText
+                    type={'email'}
+                    label={'E-mail: '}
+                    text={email}
+                    placeholder={'test@gmail.com'}
+                    inputTextCSS={'input'}
+                    required={true}
+                    action={(e) => setEmail(e.target.value)}
+                  />
+                  <Button
+                    text={'Sign up'}
+                    buttonCSS={'btn btn-primary'}
+                    action={onSubmit}
+                  />
+                  <br />
+                  Already have an account? <a href='#' onClick={() => setIsLogin(true)}>Sign in</a>
+                </>
+            }
           </Form>
         </div>
       </div>
